3c2b54f54f5d7ce7b3c0b5970ccd2afea1cb3b4a
GarVinMC <115435829+GarVinMC@users.noreply.github.com>
2025-02-04 14:24:22 +0700

    Update config.js

--- schema/config.js
+++ schema/config.js
@@ -1,16 +1,15 @@
 const options = {
-  name: "HitamAPIs",
-  developer: "@sayuran.vip",
+  name: "DasBotz Api",
+  developer: "@dastin",
   port: 3000,
-  webName: "HitamAPIs Playground",
+  webName: "Dasbotz Api Playground",
   description: "Simple Base Rest API",
   favicon:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Black.png/220px-Black.png",
 };
 
 const host = {
-  BASE_URL: "https://api-flax-chi-68.vercel.app", // Ganti dengan URL yang sesuai
-  // Contoh: https://domain.com
+  BASE_URL: "https://dasbotz.my.id/",
 };
 
 module.exports = {
