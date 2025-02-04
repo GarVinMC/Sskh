e649815b1c1b6da2ad2cdee415e4780491f19835
GarVinMC <115435829+GarVinMC@users.noreply.github.com>
2025-02-04 14:28:25 +0700

    Update config.js

--- schema/config.js
+++ schema/config.js
@@ -3,7 +3,7 @@ const options = {
   developer: "@dastin",
   port: 3000,
   webName: "Dasbotz Api Playground",
-  description: "Simple Base Rest API",
+  description: "Rest API",
   favicon:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Black.png/220px-Black.png",
 };
