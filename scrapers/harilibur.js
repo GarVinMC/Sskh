0326ffb0186b832899f81ca9b835ca67b2bff82f
GarVinMC <115435829+GarVinMC@users.noreply.github.com>
2025-02-05 14:24:14 +0700

    Update harilibur.js

--- scrapers/harilibur.js
+++ scrapers/harilibur.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 const apiKeys = [
-  'api_key_1',
-  'api_key_2',
-  'api_key_3',
-  'api_key_4',
-  'api_key_5'
+  'e1168ed6-f78e-4671-b04b-7c987c7c3607',
+  'f4bf252c-91f9-47d7-898e-c81bf60b08a4',
+  'da52f6ad-1e74-4316-aa42-447938434d62',
+  'c17343ac-14c8-4b14-b3cd-99606a8464f4',
+  '36db0f68-8812-46c1-ae0f-90a9736204fe'
 ];
 
 const getHolidayData = async (country, year) => {
