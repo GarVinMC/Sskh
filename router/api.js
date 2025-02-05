8fb39ba33c4ce36e5b89cba462dcc945341ce31d
GarVinMC <115435829+GarVinMC@users.noreply.github.com>
2025-02-05 12:38:00 +0700

    Update api.js

--- router/api.js
+++ router/api.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const danz = require("d-scrape");
 const router = express.Router();
 const config = require("../schema/config");
-const { getHolidayData } = require('../scrapers/harilibur');
 const { developer: dev } = config.options;
 
 // Log Info
@@ -29,26 +27,13 @@ const messages = {
   },
 };
 
-// Harilibur Routes
-router.get("/kalender/harilibur", async (req, res) => {
-  const { year } = req.query;
-  if (!year) return res.status(400).json(messages.query);
-
-  try {
-    const holidays = await getHolidayData('ID', year, 'e1168ed6-f78e-4671-b04b-7c987c7c3607'); // 'ID' for Indonesia and the provided API key
-    if (!holidays || holidays.length === 0) {
-      return res.status(404).json(messages.notRes);
-    }
-
-    res.json({
-      status: true,
-      developer: dev,
-      result: holidays,
-    });
-  } catch (e) {
-    console.error(e);
-    res.status(500).json(messages.error);
-  }
+// New route for testing
+router.get("/tes/tes", (req, res) => {
+  res.json({
+    status: true,
+    developer: dev,
+    result: "halo", // Your desired message
+  });
 });
 
 module.exports = router;
